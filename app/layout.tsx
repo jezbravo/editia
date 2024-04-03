365d73085c6bb81902e933c7c8da0ed3c632cf0b
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-04-03 18:51:58 -0300

    Update files

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "green" },
+      }}
+    >
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPLex.variable)}>
           {children}
