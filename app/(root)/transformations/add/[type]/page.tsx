9212917ff65137b4e54138c5854666451e4efa23
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-04-05 18:52:51 -0300

    Update files

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,30 @@
 import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constants";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
   const transformation = transformationTypes[type];
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
   return (
-    <Header title={transformation.title} subtitle={transformation.subtitle} />
+    <>
+      <Header title={transformation.title} subtitle={transformation.subtitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   );
 };
 
