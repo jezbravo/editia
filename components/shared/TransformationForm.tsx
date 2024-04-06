954da09a8350b51dcfd043694c14766d45990ed2
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-04-06 17:00:34 -0300

    Create credits modal

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -36,6 +36,7 @@ import TransformedImage from "./TransformedImage";
 import { getCldImageUrl } from "next-cloudinary";
 import { addImage, updateImage } from "@/lib/actions/image.actions";
 import { useRouter } from "next/navigation";
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -185,13 +186,14 @@ const TransformationForm = ({
     );
     setNewTransformation(null);
     startTransition(async () => {
-      await updateCredits(userId, -1);
+      await updateCredits(userId, creditFee);
     });
   };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
         <CustomField
           control={form.control}
           name="title"
