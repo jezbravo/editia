7a49a631049528b64527418ee3a5d1ac982d9f02
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-04-06 19:04:43 -0300

    Update files

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,5 +1,7 @@
-import { dataUrl, debounce, getImageSize } from "@/lib/utils";
-import { CldImage } from "next-cloudinary";
+"use client";
+
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import React from "react";
@@ -13,7 +15,20 @@ const TransformedImage = ({
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) => {
-  const downloadHandler = () => {};
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      }),
+      title,
+    );
+  };
   return (
     <div className="flex flex-col gap-4">
       <div className="flex-between">
@@ -58,7 +73,8 @@ const TransformedImage = ({
                 src="/assets/icons/spinner.svg"
                 width={50}
                 height={50}
-                alt="Transforming..."
+                alt="Transforming...
+                "
               />
             </div>
           )}
