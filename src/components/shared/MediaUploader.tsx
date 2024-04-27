08660ad9ba106bc1108006201b823fdbfc885253
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-04-27 03:47:16 -0300

    Update files

--- src/components/shared/MediaUploader.tsx
+++ src/components/shared/MediaUploader.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { NextIntlClientProvider, useLocale, useTranslations } from "next-intl";
 
 import { useToast } from "@/src/components/ui/use-toast";
 import { dataUrl, getImageSize } from "@/src/lib/utils";
@@ -32,8 +33,8 @@ const MediaUploader = ({
     }));
     onValueChange(result?.info?.public_id);
     toast({
-      title: "Image uploaded successfully",
-      description: "1 credit was deducted from your account",
+      title: t("title"),
+      description: t("description"),
       duration: 5000,
       className: "success-toast",
     });
@@ -41,56 +42,61 @@ const MediaUploader = ({
 
   const onUploadErrorHandler = () => {
     toast({
-      title: "Something went wrong while uploading",
-      description: "Please try again",
+      title: t("title-2"),
+      description: t("description-2"),
       duration: 5000,
       className: "error-toast",
     });
   };
 
+  const t = useTranslations("MediaUploader");
+  const locale = useLocale();
+
   return (
-    <CldUploadWidget
-      uploadPreset="editia"
-      options={{
-        multiple: false,
-        resourceType: "image",
-      }}
-      onSuccess={onUploadSuccessHandler}
-      onError={onUploadErrorHandler}
-    >
-      {({ open }) => (
-        <div className="flex flex-col gap-4">
-          <h3 className="h3-bold text-dark-600">Original</h3>
-          {publicId ? (
-            <>
-              <div className="cursor-pointer overflow-hidden rounded-[10px]">
-                <CldImage
-                  width={getImageSize(type, image, "width")}
-                  height={getImageSize(type, image, "height")}
-                  src={publicId}
-                  alt="image"
-                  sizes={"(max-width: 767px) 100vw, 50vw"}
-                  placeholder={dataUrl as PlaceholderValue}
-                  className="media-uploader_cldImage"
-                />
+    <NextIntlClientProvider locale={locale}>
+      <CldUploadWidget
+        uploadPreset="editia"
+        options={{
+          multiple: false,
+          resourceType: "image",
+        }}
+        onSuccess={onUploadSuccessHandler}
+        onError={onUploadErrorHandler}
+      >
+        {({ open }) => (
+          <div className="flex flex-col gap-4">
+            <h3 className="h3-bold text-dark-600">Original</h3>
+            {publicId ? (
+              <>
+                <div className="cursor-pointer overflow-hidden rounded-[10px]">
+                  <CldImage
+                    width={getImageSize(type, image, "width")}
+                    height={getImageSize(type, image, "height")}
+                    src={publicId}
+                    alt="image"
+                    sizes={"(max-width: 767px) 100vw, 50vw"}
+                    placeholder={dataUrl as PlaceholderValue}
+                    className="media-uploader_cldImage"
+                  />
+                </div>
+              </>
+            ) : (
+              <div className="media-uploader_cta" onClick={() => open()}>
+                <div className="media-uploader_cta-image">
+                  <Image
+                    src="/assets/icons/add.svg"
+                    alt="Add image"
+                    width={24}
+                    height={24}
+                  />
+                </div>
+                <p className="p-14-medium">{t("upload")}</p>
               </div>
-            </>
-          ) : (
-            <div className="media-uploader_cta" onClick={() => open()}>
-              <div className="media-uploader_cta-image">
-                <Image
-                  src="/assets/icons/add.svg"
-                  alt="Add image"
-                  width={24}
-                  height={24}
-                />
-              </div>
-              <p className="p-14-medium">Click here to upload image</p>
-            </div>
-          )}
-        </div>
-      )}
-    </CldUploadWidget>
+            )}
+          </div>
+        )}
+      </CldUploadWidget>
+    </NextIntlClientProvider>
   );
 };
 
