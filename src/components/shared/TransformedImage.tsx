08930eec594d6d6399d984ca2339016d5b29d61b
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-04-27 03:31:45 -0300

    Update files

--- src/components/shared/TransformedImage.tsx
+++ src/components/shared/TransformedImage.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { NextIntlClientProvider, useLocale, useTranslations } from "next-intl";
 
 import { dataUrl, debounce, download, getImageSize } from "@/src/lib/utils";
 import { CldImage, getCldImageUrl } from "next-cloudinary";
@@ -29,62 +30,65 @@ const TransformedImage = ({
       title,
     );
   };
+  const t = useTranslations("TransformedImage");
+  const locale = useLocale();
+
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex-between">
-        <h3 className="h3-bold text-dark-600">Transformed</h3>
+    <NextIntlClientProvider locale={locale}>
+      <div className="flex flex-col gap-4">
+        <div className="flex-between">
+          <h3 className="h3-bold text-dark-600">{t("transformed")}</h3>
 
-        {hasDownload && (
-          <button className="download-btn" onClick={downloadHandler}>
-            <Image
-              src="/assets/icons/download.svg"
-              alt="Download"
-              width={24}
-              height={24}
-              className="pb-[6px]"
-            />
-          </button>
-        )}
-      </div>
-
-      {image?.publicId && transformationConfig ? (
-        <div className="relative">
-          <CldImage
-            width={getImageSize(type, image, "width")}
-            height={getImageSize(type, image, "height")}
-            src={image?.publicId}
-            alt={image.title}
-            sizes={"(max-width: 767px) 100vw, 50vw"}
-            placeholder={dataUrl as PlaceholderValue}
-            className="transformed-image"
-            onLoad={() => {
-              setIsTransforming && setIsTransforming(false);
-            }}
-            onError={() => {
-              debounce(() => {
-                setIsTransforming && setIsTransforming(false);
-              }, 8000)();
-            }}
-            {...transformationConfig}
-          />
-          {isTransforming && (
-            <div className="transforming-loader">
+          {hasDownload && (
+            <button className="download-btn" onClick={downloadHandler}>
               <Image
-                src="/assets/icons/spinner.svg"
-                width={50}
-                height={50}
-                alt="spinner"
+                src="/assets/icons/download.svg"
+                alt="Download"
+                width={24}
+                height={24}
+                className="pb-[6px]"
               />
-              <p className="text-white/80">
-                Transformation in progress. Please wait...
-              </p>
-            </div>
+            </button>
           )}
         </div>
-      ) : (
-        <div className="transformed-placeholder">Transformed Image</div>
-      )}
-    </div>
+
+        {image?.publicId && transformationConfig ? (
+          <div className="relative">
+            <CldImage
+              width={getImageSize(type, image, "width")}
+              height={getImageSize(type, image, "height")}
+              src={image?.publicId}
+              alt={image.title}
+              sizes={"(max-width: 767px) 100vw, 50vw"}
+              placeholder={dataUrl as PlaceholderValue}
+              className="transformed-image"
+              onLoad={() => {
+                setIsTransforming && setIsTransforming(false);
+              }}
+              onError={() => {
+                debounce(() => {
+                  setIsTransforming && setIsTransforming(false);
+                }, 8000)();
+              }}
+              {...transformationConfig}
+            />
+            {isTransforming && (
+              <div className="transforming-loader">
+                <Image
+                  src="/assets/icons/spinner.svg"
+                  width={50}
+                  height={50}
+                  alt="spinner"
+                />
+                <p className="text-white/80">{t("Progress")}</p>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="transformed-placeholder">{t("Transformed")}</div>
+        )}
+      </div>
+    </NextIntlClientProvider>
   );
 };
 
